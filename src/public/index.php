40f965a2233eae19801b4a533b54b5c8ae0980d0
maeldsoncavalcante100 <maeldson2015@gmail.comm>
2023-04-26 11:08:09 -0300

    primeiro commit da aula do dia 26/04/2023

--- src/public/index.php
+++ src/public/index.php
@@ -1,3 +1,44 @@
 <?php
 
-phpinfo();
+//phpinfo();
+
+//echo 'oi';
+
+//declare(strict_types=1);
+
+//$contador = 1;
+//$mensagem = "Olá";
+//$versao = array('1.1'. '2', '3.4');
+//print_r($versao);
+//$primeiro_nome = "Maria";
+//echo $primeiro_nome;
+//echo '<br />';
+//$ultimo_nome = "Lourdes";
+//echo $ultimo_nome;
+
+//$time = array('Maria', 'José', 'Ana');
+
+//echo $time[0];
+
+//echo '<br />';
+//print_r($time)
+
+//echo 'a: [' . TRUE . ']<br />';
+//echo 'a: [' . FALSE . ']<br />';
+//$idade = 30;
+
+//echo 'a: [' . (20 > 9) . ']<br />';
+//echo 'b: [' . (5 == 6) . ']<br />';
+//echo 'c: [' . (1 == 0) . ']<br />';
+//echo 'd: [' . (1 === '1') . ']<br />';
+//echo 'e: [' . '$idade'. ']<br />';
+$saldo = 10;
+if ($saldo<100) {
+    $deposito = 1000;
+    $saldo += $deposito;
+}
+
+//echo $saldo
+
+
+?>
